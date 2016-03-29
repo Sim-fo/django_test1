9b84df25aedc3bf2110f4786b6903a140e3a2bfc
Sim-fo <simfo@bk.ru>
2016-03-29 12:34:23 +0600

    Лайки прикрутил,  картинки пока не работают

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Post(models.Model):
+    class Meta():
+        db_table = 'post'
     post_title = models.CharField(max_length=140)
     post_body = models.TextField()
     post_date = models.DateTimeField()
+    post_like = models.IntegerField()
+    post_img = models.ImageField()
 
     def __str__(self):
         return self.post_title
