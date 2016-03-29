3fc8d04967ead1f1ea8b0192167adb1bbf33eeee
Sim-fo <simfo@bk.ru>
2016-03-29 11:54:16 +0600

    переменные и бд изменил, все работает

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Post(models.Model):
     post_title = models.CharField(max_length=140)
-    body = models.TextField()
-    date = models.DateTimeField()
+    post_body = models.TextField()
+    post_date = models.DateTimeField()
 
     def __str__(self):
         return self.post_title
