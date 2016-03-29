41510cd120683a53303dc3c10948655d09e3e8b6
Sim-fo <simfo@bk.ru>
2016-03-29 12:52:59 +0600

    Лайки прикрутил,  картинки пока не работают

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     post_body = models.TextField()
     post_date = models.DateTimeField()
     post_like = models.IntegerField()
-    post_img = models.ImageField()
+    post_img = models.ImageField(upload_to='media/images/', blank=True, null=True)
 
     def __str__(self):
         return self.post_title
